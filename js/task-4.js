aedd7a3b83b8fd5f702edf50870989c2d729d4b4
Anna Kharytonova <haritonova1512@gmail.com>
2024-11-04 22:45:20 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const elements = event.target.elements;
+  const email = elements.email.value.trim();
+  const password = elements.password.value.trim();
+
+  if (email === '' || password === '') {
+    return alert('All form fields must be filled in');
+  }
+
+  const info = {
+    email: email,
+    password: password,
+  };
+
+  console.log(info);
+  event.target.reset();
+}
