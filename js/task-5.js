7c1370ecdfd222d61952e463de0d5e4f148e78cf
Anna Kharytonova <haritonova1512@gmail.com>
2024-11-05 20:09:22 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+const body = document.querySelector('body');
+
+btn.addEventListener('click', changeColorBody);
+
+function changeColorBody() {
+  const randomColor = getRandomHexColor();
+
+  return (body.style.backgroundColor = span.textContent = randomColor);
+}
